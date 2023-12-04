cd9267234dceb86c56e0363939b39dfbf827f030
Matthew Toohey <contact@mtoohey.com>
2023-12-03 20:08:19 -0500

    day03

---
+++ day03/haskell/part1/Main.hs
@@ -0,0 +1,58 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Functor law" #-}
+{-# HLINT ignore "Redundant <&>" #-}
+
+import Data.Array (bounds, listArray, (!))
+import Data.Char (isDigit, ord)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+
+data Cell = Digit Int | Symbol | Empty deriving (Eq)
+
+toCell c = case c of
+  c | isDigit c -> Digit (ord c - ord '0')
+  '.' -> Empty
+  _ -> Symbol
+
+toSchema lines =
+  concatMap (map toCell) lines & listArray ((1, 1), (length lines, head lines & length))
+
+hasAdjacentSymbol schema (row, (colStart, colEnd)) =
+  filter
+    inBounds
+    ( (row, colStart - 1)
+        : (row, colEnd + 1)
+        : [(row - 1, col) | col <- [colStart - 1 .. colEnd + 1]]
+        ++ [(row + 1, col) | col <- [colStart - 1 .. colEnd + 1]]
+    )
+    & any
+      ((== Symbol) . (schema !))
+  where
+    ((rowMin, colMin), (rowMax, colMax)) = bounds schema
+    inBounds (row, col) = rowMin <= row && row <= rowMax && colMin <= col && col <= colMax
+
+rowPartNumbersSum schema row = rowPartNumbersSum' colMin
+  where
+    ((_, colMin), (_, colMax)) = bounds schema
+    rowPartNumbersSum' col | col > colMax = 0
+    rowPartNumbersSum' col = case schema ! (row, col) of
+      Digit _ ->
+        let (number, colEnd) = readNumber col
+         in if hasAdjacentSymbol schema (row, (col, colEnd))
+              then number + rowPartNumbersSum' (colEnd + 1)
+              else rowPartNumbersSum' (col + 1)
+      _ -> rowPartNumbersSum' (col + 1)
+    readNumber col | col > colMax = (0, colMax)
+    readNumber col = case schema ! (row, col) of
+      Digit i ->
+        let (numberRest, colMax) = readNumber (col + 1)
+         in (i * (10 ^ (colMax - col)) + numberRest, colMax)
+      _ -> (0, col - 1)
+
+partNumbersSum schema = map (rowPartNumbersSum schema) [rowMin .. rowMax] & sum
+  where
+    ((rowMin, _), (rowMax, _)) = bounds schema
+
+main = do
+  readFile "../../input" <&> lines <&> toSchema <&> partNumbersSum >>= print
---
+++ day03/haskell/part2/Main.hs
@@ -0,0 +1,57 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Redundant <&>" #-}
+{-# HLINT ignore "Functor law" #-}
+
+import Data.Array (bounds, indices, listArray, (!))
+import Data.Char (isDigit, ord)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.List (nub)
+
+data Cell = Digit Int | Star | Other deriving (Eq)
+
+toCell c = case c of
+  c | isDigit c -> Digit (ord c - ord '0')
+  '*' -> Star
+  _ -> Other
+
+toSchema lines =
+  concatMap (map toCell) lines & listArray ((1, 1), (length lines, head lines & length))
+
+starIndices schema = indices schema & filter ((== Star) . (schema !))
+
+gearRatio schema (row, col) =
+  let gears =
+        filter
+          inBounds
+          ( (row, col - 1)
+              : (row, col + 1)
+              : [(row - 1, col) | col <- [col - 1 .. col + 1]]
+              ++ [(row + 1, col) | col <- [col - 1 .. col + 1]]
+          )
+          & filter (isDigit . (schema !))
+          & map readNumber
+          & nub
+   in if length gears < 2 then 0 else map fst gears & product
+  where
+    ((rowMin, colMin), (rowMax, colMax)) = bounds schema
+    inBounds (row, col) = rowMin <= row && row <= rowMax && colMin <= col && col <= colMax
+    readNumber (row, col) | col < colMin = readNumber' (row, colMin)
+    readNumber (row, col) = case schema ! (row, col) of
+      Digit _ -> readNumber (row, col - 1)
+      _ -> readNumber' (row, col + 1)
+    readNumber' (_, col) | col > colMax = (0, colMax)
+    readNumber' (row, col) = case schema ! (row, col) of
+      Digit i ->
+        let (numberRest, colMax) = readNumber' (row, col + 1)
+         in (i * (10 ^ (colMax - col)) + numberRest, colMax)
+      _ -> (0, col - 1)
+    isDigit c = case c of
+      Digit _ -> True
+      _ -> False
+
+gearRatiosSum schema = starIndices schema & map (gearRatio schema) & sum
+
+main = do
+  readFile "../../input" <&> lines <&> toSchema <&> gearRatiosSum >>= print
